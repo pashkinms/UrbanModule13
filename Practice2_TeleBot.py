48d8b5ae56991036687f7220474b2fbf3178d8b2
AlcheMystic <pashkinms86@gmail.com>
2024-12-18 04:42:51 +0700

    Task2_complete an added to git

---
+++ Practice2_TeleBot.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+import asyncio
+
+api = '7718220463:AAFVziZE_NMg1UVysjlqTEv7krauQm7Ysi4'
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands='start')
+async def start_message(message):
+    print("I'm bot to help you with your health")
+
+@dp.message_handler()
+async def all_message(message):
+    print('Enter /start command to start messaging!')
+
+    
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
+ 
