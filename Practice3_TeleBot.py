b04090b5c98f4f6bf7ec05e469fe2feb4f3be2cb
AlcheMystic <pashkinms86@gmail.com>
2024-12-18 05:13:31 +0700

    Task3 Done!

--- Practice3_TeleBot.py
+++ Practice3_TeleBot.py
@@ -10,15 +10,14 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands='start')
 async def start_message(message):
-    print("I'm bot to help you with your health")
-    await message.answer('Start me, baby, one more time!')
+    await message.answer("I'm bot to help you with your health")
     
 @dp.message_handler()
 async def all_message(message):
-    print('Enter /start command to start messaging!')
-    await message.answer(f'{message.text.upper()}!!!')
+    await message.answer('Enter /start command to start messaging!')
     
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
 
  
+
